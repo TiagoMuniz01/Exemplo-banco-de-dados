8c91496d1b688e370a5ac97d1db7f55505480ce4
Tiago Pimentel Muniz <131329736+TiagoMuniz01@users.noreply.github.com>
2024-05-28 11:40:31 -0300

    arrumando arquivos das aulas anteriores

--- exemplo_banco_de_dados/controller/conexao.php
+++ exemplo_banco_de_dados/controller/conexao.php
@@ -1,23 +1,27 @@
 <?php
 
-class Conexao{
+//classe publica por padrão
+class Conexao {
+    //atributos privados da classe
     private $host = "localhost";
     private $usuario = "root";
     private $senha = "";
     private $banco = "exemplo_aula_pw";
     private $conexao;
-    
 
-    public function __construct(){
+    public function __construct() {
+        //mysqli: propriedades do mysql
         $this->conexao = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
 
         if ($this->conexao->connect_error) {
-            die("Falha na conexão: ".$this->conexao->connect_error);
+            //die: falha sistemica
+            die("Falha na Conexão: " . $this->conexao->connect_error);
         }
     }
 
-    public function getConexao() {
+    public function getConexao(){
         return $this->conexao;
     }
 }
+
 ?>
